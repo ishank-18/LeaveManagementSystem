08662770345eab97a8337d0e2f87a0af3c81d0d7
Ishank Rajput <ishank.rajput@alteryx.com>
2025-10-30 13:10:46 +0530

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Computer",
+                DateofBirth = new DateTime(1999, 11, 5)
+            };
+            return View(data);
+        }
+    }
+}
